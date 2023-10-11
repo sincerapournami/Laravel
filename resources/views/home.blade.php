957a3d3a066b026b804f7242197969629849682b
Pournami <pournami@sincera.in>
2023-10-11 16:49:25 +0530

    New Project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Project</title>
+</head>
+<body>
+<div class="container">
+<header>
+<ul class="navigation">
+    <li><a href="/">Home</a></li>
+    <li><a href="/register">Register</a></li>
+</ul>
+</header>
+</div>
+</body>
+</html>
